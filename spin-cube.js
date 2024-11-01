a18af3dcb7da8af2db2acdea3610b53858949959
all <pgodunov@kait20.ru>
2024-11-01 10:43:13 +0300

    Delete spin-cube.js

--- spin-cube.js
+++
@@ -1,9 +0,0 @@
-const cube = document.getElementById("cube");
-let rotation = 0;
-
-function rotateCube() {
-  rotation += 1;
-  cube.style.transform = `rotateY(${rotation}deg)`;
-}
-
-setInterval(rotateCube, 25);
